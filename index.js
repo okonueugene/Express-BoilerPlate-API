b5424931f6e6fef788adaf449da26d5aba7a62e9
okonueugene <tylarrex67@gmail.com>
2023-01-09 17:01:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.delete("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
-app.get("/api/courses/:name", (req, res, next) => {
+app.get("/api/course/:name", (req, res, next) => {
   const filters = req.params.name;
 
   if(filters.length === 0) return res.status(400).send("Bad Request");
